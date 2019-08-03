5acbe1c42a4af7edcdce10bd6c5f156bdba51753
Malik Naik <maliknaik16@gmail.com>
2019-08-04 02:52:35 +0530

    Initial Unity project

---
+++ Assets/Scripts/ArrowCollisionCheck.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowCollisionCheck : MonoBehaviour
+{
+    [SerializeField] private ArrowManager arrowManager;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        arrowManager.CollisionCount++;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        arrowManager.CollisionCount--;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    [SerializeField] private Text text; 
+    public float Score = 0;
+    public float GlobalSpeed;
+
+    private void Start()
+    {
+        
+    }
+
+    public IEnumerator AddToScore(int value)
+    {
+        for (int i = 0; i < value; i++)
+        {
+            yield return new WaitForSeconds(0.001f);
+            Score++;
+            text.text = "$ " + Score;
+        }
+    }
+}
