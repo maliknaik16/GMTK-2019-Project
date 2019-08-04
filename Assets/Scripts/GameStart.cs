a9d275972a81128ad4486aa8234938cc3711c0c8
maliknaik16 <maliknaik16@gmail.com>
2019-08-04 21:17:43 +0530

    Add menu

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStart : MonoBehaviour
+{
+	private void Update() {
+		if(Input.anyKeyDown) {
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+	}
+}
