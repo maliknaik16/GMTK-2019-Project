ecd36beafbf1dc0d41d3ee4aab900ec68eb6206a
Quoc <QTran.NT@gmail.com>
2019-08-04 20:23:27 +0200

    Changed the art to new art

---
+++ Assets/GameEnd.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnd : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    private void Update()
+    {
+        if (GameManager.instance.startPlaying)
+        {
+            if (!audioSource.isPlaying)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    }
+}
